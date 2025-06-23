aa52d47742b1f93314ec4b4ebe0b5622d47d3ec1
emazack <em.zaccaria@gmail.com>
2025-06-23 09:59:41 +0200

    [P2] Header component

--- src/components/Sections/Header/Header.tsx
+++ src/components/Sections/Header/Header.tsx
@@ -1,6 +1,24 @@
 import React from "react";
 import styles from "./Header.module.scss";
 
-const Header = () => <div className={styles.header}>Header Placeholder</div>;
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            <h1 className={styles.title}>emanuelezaccaria</h1>
+
+            <nav className={styles.nav} aria-label="Social media">
+                <ul>
+                    <li>
+                        <a href="#github">GitHub</a>
+                    </li>
+                    <li>
+                        <a href="#linkedin">LinkedIn</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+
+    );
+}
 
 export default Header;
