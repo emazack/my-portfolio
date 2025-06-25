853f06a0b9db087af29250115165b28aaaea50c1
emazack <em.zaccaria@gmail.com>
2025-06-25 13:03:27 +0200

    [P1] Header and first step Hero

--- src/components/Sections/Hero/Hero.tsx
+++ src/components/Sections/Hero/Hero.tsx
@@ -1,6 +1,40 @@
 import React from "react";
 import styles from "./Hero.module.scss";
+import Image from "next/image";
 
-const Hero = () => <div className={styles.hero}>Hero Placeholder</div>;
+const Hero = () => {
+    return (
+        <section className={styles.hero}>
+            <div className={styles.container}>
+                <div className={styles.text}>
+                    <h1 className={styles.title}>
+                        Nice to meet you! I'm 
+                        <span className={styles.highlight}>
+                            Emanuele
+                        </span>
+                        .
+                    </h1>
+                    <p className={styles.description}>
+                        Based in the UK, I’m a front-end developer passionate about building accessible web apps that users love.
+                    </p>
+                    <div className={styles.cta}>
+                        <a>
+                            CONTACT ME
+                        </a>
+                    </div>
+                </div>
+                <div className={styles.image}>
+                    <Image
+                        src="/foto.png"
+                        alt="Hero Image"
+                        width={445}
+                        height={720}
+                        className={styles.heroImage}
+                    />
+                </div>
+            </div>
+        </section>
+    );
+}
 
 export default Hero;
