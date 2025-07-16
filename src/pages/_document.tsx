0d0a8514b2692a789e717263fd250db422b96f23
Emanuele Zaccaria <em.zaccaria@gmail.com>
2025-07-16 16:52:58 +0200

    [P1] Small fix, wrap up

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html data-dark-mode lang="en">
       <Head>
         <link
           href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@500;700&display=swap"
